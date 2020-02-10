2c543f08edf04fa6a687ec15ac47fb2257541a5e
yusuke1117 <60721154+yusuke1117@users.noreply.github.com>
2020-02-10 09:39:05 +0800

    [Layout] Sign-up (#2)
    * [Layout] Sign-up
    
    * [Layout] Sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'users/new'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  resources :users
   get '/signup', to:'users#new'
   
 end

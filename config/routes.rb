dc06a463f2ca0fa6c1036011aa3d7fe7c8589225
yusuke1117 <60721154+yusuke1117@users.noreply.github.com>
2020-02-11 09:13:24 +0800

    [Layout] login (#4)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users
   get '/signup', to:'users#new'
-  
+  get '/login', to:'sessions#new'
+
 end

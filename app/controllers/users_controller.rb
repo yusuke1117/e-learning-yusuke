b0b1337a75dbda9a60d36f375982f2bf03d26bdb
yusuke1117 <iysk.iwaisk@gmail.com>
2020-02-12 10:24:47 +0800

    [Function] edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,6 +30,6 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,5 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, length: { minimum: 6 }
+  mount_uploader :avatar, AvatarUploader
 end

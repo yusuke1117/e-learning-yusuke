7de6d7c20949ec1ce330815c4479d9ad2e9cf018
yusuke1117 <iysk.iwaisk@gmail.com>
2020-02-13 09:58:12 +0800

    [Layout] New Category page

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,7 @@
+class Admin::CategoriesController < ApplicationController
+  def new
+  end
+
+  def index
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get '/login', to:'sessions#new'
   delete '/logout', to: 'sessions#destroy'
   
+  namespace :admin do
+    resources :categories
+  end
+  
 end

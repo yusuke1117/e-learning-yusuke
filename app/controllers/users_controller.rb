9a8a765797ab8b92ecde3229cdfa9b5ec41ffaa5
yusuke1117 <iysk.iwaisk@gmail.com>
2020-02-28 12:27:46 +0800

    [Layout] Display Activities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,8 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+    @activities = @user.activities.where(user: @user).order(created_at: :desc).paginate(page: params[:page], per_page: 6)
   end
 
   def edit

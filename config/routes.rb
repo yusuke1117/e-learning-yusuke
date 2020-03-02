24ea79ccd2c2b7224f02811d66464193eba4839d
yusuke1117 <iysk.iwaisk@gmail.com>
2020-03-02 12:38:41 +0800

    [Layout] Admin Users List

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-
-
+  
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
@@ -22,5 +21,6 @@ Rails.application.routes.draw do
     resources :categories do
       resources :words
     end
+    resources :users
   end
 end
